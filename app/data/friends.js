a922902d54a9c8c0c33e3f89dd2dcdd81cbcf978
beshoyr4 <33008284+beshoyr4@users.noreply.github.com>
2019-03-24 15:53:37 -0500

    1st push

---
+++ app/data/friends.js
@@ -0,0 +1,90 @@
+var friendArray = [
+    {
+      name: "Jim",
+      photo: "./assets/images/Jim.jpg",
+      "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+    },
+
+    {
+      name: "Judy",
+      photo: "./assets/images/Judy.jpg",
+      "scores":[
+        2,
+        4,
+        1,
+        1,
+        2,
+        5,
+        5,
+        4,
+        2,
+        3
+      ]
+    },
+
+    {
+      name: "Jared",
+      photo: "./assets/images/Jared.jpg",
+      "scores":[
+        1,
+        1,
+        5,
+        5,
+        3,
+        3,
+        4,
+        4,
+        3,
+        2
+      ]
+    },
+
+    {
+      name: "Jill",
+      photo: "./assets/images/Jill.jpg",
+      "scores":[
+        4,
+        4,
+        3,
+        1,
+        5,
+        2,
+        3,
+        2,
+        5,
+        5
+      ]
+    },
+
+    {
+      name: "Jenna",
+      photo: "./assets/images/Jenna.jpg",
+      "scores":[
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+      ]
+    }
+
+  ];
+  
+  // Export the array to make it accessible to other files using require.
+  module.exports = friendArray;
